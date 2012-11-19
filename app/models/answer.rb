ef7f384ac64ba8b199a341c9cee24423e46a351b
Jeff Smith <jeff@smith>
2012-11-19 09:56:06 -0800

    final ui changes; ready for production

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ActiveRecord::Base
   attr_accessible :question_id, :content, :author
 
-  validates :content, :presence => { :message => "An author is required" }
+  validates :content, :presence => { :message => "is required for submission" }
 
   belongs_to :question
   delegate :poll, :to => :question
