6cc905543b59b97d103f28643dec8faa6d083e24
Jeff Smith <jeff@smith>
2012-11-18 21:41:47 -0800

    add minor changes to controllers and views, begin refactoring into partials

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,7 +6,13 @@ class AnswersController < ApplicationController
       @answer = question.answers.new(params[:answer])
       if @answer.save
         answered_questions << @answer.question_id
+      # else
+      #  render action: "polls/answer_fields" 
+      # this is where you will render a new question partial
+     
       end
+      
+
     end
 
     redirect_to poll_path(question.poll)
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ActiveRecord::Base
   attr_accessible :question_id, :content, :author
 
+  validates :content, :presence => { :message => "An author is required" }
+
   belongs_to :question
   delegate :poll, :to => :question
 end
