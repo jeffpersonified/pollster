afd1e6fd04fed80f15f6ee361530107289205f5e
Jeff Smith <jeff@smith>
2012-11-18 23:37:14 -0800

    fix Zurb gem inconsistencies and add flash message for empty answer validation

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,15 +6,10 @@ class AnswersController < ApplicationController
       @answer = question.answers.new(params[:answer])
       if @answer.save
         answered_questions << @answer.question_id
-      # else
-      #  render action: "polls/answer_fields" 
-      # this is where you will render a new question partial
-     
+      else
+        flash[:error] = @answer.errors.to_a.join(', ')
       end
-      
-
     end
-
     redirect_to poll_path(question.poll)
   end
 end
